c5f0574926247bf55d3563d531dae12a4f7c6a55
Danny <dl667@cornell.edu>
2022-10-20 16:57:34 -0400

    working on license section

---
+++ Develop/README.md
@@ -0,0 +1,27 @@
+# Danny Lee's Professional ReadME Generator
+## Motivation
+
+To make a tool to help me make professional READMEs
+
+## The Why
+
+I can use this anytime in the future for all my projects.
+
+## What it Solves
+
+It solves the need to have to make a new README every project.
+
+## Learning Outcomes
+
+How to create professional READMEs and how to use JavaScript to create files.
+
+## Contact Info
+
+Please do not hesitate to reach out if you have any questions, comments, concerns, or opportunities.
+
+You can reach me through LinkedIn at https://linkedin.com/in/daniel-jun-lee/
+or through GitHub at https://github.com/dannyjunlee/.
+
+## Licenses
+
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const {prompt} = require('inquirer');
-const {writeFile} = require('fs');
-const generateMarkdown = require('./potential-enigma/Develop/utils/generateMarkdown')
+const {writeFile, appendFile} = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const renderLicenseSection = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const userInput = (message, required = false) =>
@@ -19,9 +20,6 @@ const userInput = (message, required = false) =>
     }])
     .then(answer => answer.val);
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
 async function init() {
     console.log("Welcome to the Professional README Generator!\n");
@@ -30,19 +28,27 @@ async function init() {
     const data = {
         yourName: await userInput("What is your name?", true),
         projName: await userInput("What is the name of the project?", true),
+        license: await userInput("What is the license used?", true),
         linkedIn: await userInput("What is your LinkedIn ID?"),
         gitHub: await userInput("What is your GitHub ID?", true),
         motivation: await userInput("What was your motivation?\n", true),
         why: await userInput("Why did you build this project?\n", true),
         whatSolve: await userInput("What problem does it solve?\n", true),
-        whatLearn: await userInput("What did you learn?\n", true)
+        whatLearn: await userInput("What did you learn?\n", true),
     };
 
-    const readme = generateMarkdown(data);
+    const {licenseUsed} = data;
 
+    const readme = generateMarkdown(data);
+    const licenseSection = renderLicenseSection(licenseUsed)
+    
+    // TODO: Create a function to write README file
     writeFile('README.md', readme, err => {
         err ? console.log(err.message) : console.log("Success! Your README has been created.");
     });
+    appendFile('README.md', licenseSection, (err) =>
+        err ? console.error(err) : console.log('License section appended!')
+    );
 };
 
 // Function call to initialize app
